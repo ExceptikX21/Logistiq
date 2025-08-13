66c7b29555a7494a5c6bc54f0e6a5ca988c73b2a
Valencia CEO <solutionslogistiq@gmail.com>
2025-08-13 09:53:10 -0500

    Mantener solo frontend para Vercel

--- durmanback/helpers/roles.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    rolesAdmin: ['admin_basico', 'admin_pro', 'admin_empresarial'],
-    rolesPro: ['admin_pro', 'admin_empresarial'],
-    rolesDemo: ['usuario_demo'],
-    rolesAll: ['usuario_demo', 'admin_basico', 'admin_pro', 'admin_empresarial'],
-  };
-  
